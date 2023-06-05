045ae045a11ffcf6c8b157b516af6a307fd2fe38
Medford Adelso Sotz perez <94406370+Medfordsotz@users.noreply.github.com>
2023-06-05 12:16:58 -0600

    Revert "rama sots"
    Terminado mantenimiento a insumos

--- webpack.config.js
+++ webpack.config.js
@@ -6,16 +6,6 @@ module.exports = {
   entry: {
     'js/app' : './src/js/app.js',
     'js/inicio' : './src/js/inicio.js',
-    'js/insumo/index' : './src/js/insumo/index.js',
-    'js/operaciones/index' : './src/js/operaciones/index.js',
-    'js/medios/index' : './src/js/medios/index.js',
-    'js/receptores/index' : './src/js/receptores/index.js',
-    'js/tipo_embarcaciones/index' : './src/js/tipo_embarcaciones/index.js',
-    'js/embarcaciones/index' : './src/js/embarcaciones/index.js',
-    'js/motores/index' : './src/js/motores/index.js',
-    'js/unidad_medida/index' : './src/js/unidad_medida/index.js',
-    
-    
   },
   output: {
     filename: '[name].js',
